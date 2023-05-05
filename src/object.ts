f413c78a1beb4019e27ce60015f60902477f41d9
Nirob Hasan <nirob427417@gmail.com>
2023-05-05 11:07:42 +0600

    index & object

---
+++ src/object.ts
@@ -0,0 +1,19 @@
+class Animal {
+    // public name: string;
+    // public species: string;
+    // public sound: string;
+
+    constructor(
+        public name: string,
+        public species: string,
+        public sound: string) {
+        // this.name = name;
+        // this.species = species;
+        // this.sound = sound;
+    }
+    public makeSound() {
+        console.log(`this ${this.name} says ${this.sound}`);
+    }
+}
+const dog = new Animal('dogi', 'dog', 'ghaw ghaw');
+dog.makeSound()
